f6290cfd9082ac42f2d74449bfd55c63f8954ace
Chandler Newman <chandler2newman@hotmail.co.uk>
2023-12-29 16:54:41 +0000

    Rework dart bindings to use native assets

--- generator/dart.go
+++ generator/dart.go
@@ -28,9 +28,8 @@ final class BridgeException implements Exception {
 }
 
 abstract interface class Bridge {
-  factory Bridge.open(ffi.DynamicLibrary lib) {
-    // TODO: Auto configure
-    return _FfiBridge(lib);
+  factory Bridge.open() {
+    return _FfiBridge();
   }
 {{range $f := $top.Functions}}
   {{if $f.HasRes}}{{$f.Res.DartType}}{{else}}void{{end}} {{$f.CamelName}}(
@@ -57,6 +56,12 @@ final class _Ffi{{$s.PascalName}} implements {{$s.PascalName}}, ffi.Finalizable 
     return '{{$s.PascalName}}(${id.address})';
   }
 }
+
+@ffi.Native<ffi.Void Function(ffi.Pointer<ffi.Void>)>(symbol: "fgbfree_{{$s.SnakeName}}")
+external void _fgbIntFree{{$s.PascalName}}(ffi.Pointer<ffi.Void> arg0);
+
+ffi.Pointer<ffi.NativeFinalizerFunction> _free{{$s.PascalName}}Ptr = ffi.Native.addressOf(_fgbIntFree{{$s.PascalName}});
+
 {{- end}}
 {{- range $s := $top.ValueStructs}}
 
@@ -94,26 +99,25 @@ final class _FgbC{{$s.PascalName}} extends ffi.Struct {
 {{- end}}
 }
 
-typedef _FgbEmpty{{$s.PascalName}} = _FgbC{{$s.PascalName}} Function();
+@ffi.Native<_FgbC{{$s.PascalName}} Function()>(symbol: "fgbempty_{{$s.SnakeName}}")
+external _FgbC{{$s.PascalName}} _fgbEmpty{{$s.PascalName}}();
 {{- end}}
 
-typedef _FgbDefInit = ffi.Pointer<ffi.Void> Function(ffi.Pointer<ffi.Void>);
-typedef _FgbDefIntCAlloc = ffi.Pointer Function(ffi.IntPtr);
-typedef _FgbDefIntDartAlloc = ffi.Pointer Function(int);
-typedef _FgbDefIntCFree = ffi.Void Function(ffi.Pointer);
-typedef _FgbDefIntDartFree = void Function(ffi.Pointer);
-typedef _NativeFinalizerFunctionC = ffi.Void Function(ffi.Pointer<ffi.Void>);
-typedef _NativeFinalizerFunctionDart = void Function(ffi.Pointer<ffi.Void>);
+@ffi.Native<ffi.Pointer<ffi.Void> Function(ffi.Pointer<ffi.Void>)>(symbol: "fgbinternal_init")
+external ffi.Pointer<ffi.Void> _fgbInternalInit(ffi.Pointer<ffi.Void> arg0);
+
+@ffi.Native<ffi.Pointer Function(ffi.IntPtr)>(symbol: "fgbinternal_alloc")
+external ffi.Pointer _fgbInternalAlloc(int arg0);
+
+@ffi.Native<ffi.Void Function(ffi.Pointer)>(symbol: "fgbinternal_free")
+external void _fgbInternalFree(ffi.Pointer arg0);
 
 class _GoAllocator implements ffi.Allocator {
-  final _FgbDefIntDartAlloc _allocPtr;
-  final _FgbDefIntDartFree _freePtr;
-
-  const _GoAllocator(this._allocPtr, this._freePtr);
+  const _GoAllocator();
 
   @override
   ffi.Pointer<T> allocate<T extends ffi.NativeType>(int byteCount, {int? alignment}) {
-    ffi.Pointer<T> result = _allocPtr(byteCount).cast();
+    ffi.Pointer<T> result = _fgbInternalAlloc(byteCount).cast();
     if (result.address == 0) {
       throw ArgumentError('Could not allocate $byteCount bytes.');
     }
@@ -122,7 +126,7 @@ class _GoAllocator implements ffi.Allocator {
 
   @override
   void free(ffi.Pointer pointer) {
-  	_freePtr(pointer);
+  	_fgbInternalFree(pointer);
   }
 }
 
@@ -142,17 +146,7 @@ final class _FgbRet{{$f.PascalName}} extends ffi.Struct {
   external ffi.Pointer<ffi.Void> err;
 }
 
-typedef _FgbDefDart{{$f.PascalName}} = _FgbRet{{$f.PascalName}} Function(
-  {{- range $i, $p := $f.Params}}{{if gt $i 0}}, {{end}}
-{{- if eq $p.DartMode "direct" -}}
-  {{$p.DartType}}
-{{- else if eq $p.DartMode "map" -}}
-  {{$p.DartCType}}
-{{- else -}}
-  unknown
-{{- end}}{{end -}}
-);
-typedef _FgbDefC{{$f.PascalName}} = _FgbRet{{$f.PascalName}} Function(
+@ffi.Native<_FgbRet{{$f.PascalName}} Function(
   {{- range $i, $p := $f.Params}}{{if gt $i 0}}, {{end}}
 {{- if eq $p.DartMode "direct" -}}
   {{$p.DartCType}}
@@ -161,53 +155,52 @@ typedef _FgbDefC{{$f.PascalName}} = _FgbRet{{$f.PascalName}} Function(
 {{- else -}}
   unknown
 {{- end}}{{end -}}
-);
-typedef _FgbAsyncDefDart{{$f.PascalName}} = void Function(
-  {{- range $p := $f.Params}}{{- if eq $p.DartMode "direct" -}}
+)>(symbol: "fgb_{{$f.SnakeName}}")
+external _FgbRet{{$f.PascalName}} _fgb{{$f.PascalName}}(
+  {{- range $i, $p := $f.Params}}{{if gt $i 0}}, {{end}}
+{{- if eq $p.DartMode "direct" -}}
   {{$p.DartType}}
 {{- else if eq $p.DartMode "map" -}}
   {{$p.DartCType}}
 {{- else -}}
   unknown
-{{- end}}, {{end -}}
-int);
-typedef _FgbAsyncDefC{{$f.PascalName}} = ffi.Void Function(
-  {{- range $p := $f.Params}}{{- if eq $p.DartMode "direct" -}}
+{{- end}} arg{{$i}}{{end -}}
+);
+
+@ffi.Native<ffi.Void Function(
+{{- range $p := $f.Params}}{{- if eq $p.DartMode "direct" -}}
   {{$p.DartCType}}
 {{- else if eq $p.DartMode "map" -}}
   {{$p.DartCType}}
 {{- else -}}
   unknown
 {{- end}}, {{end -}}
-ffi.Uint64);
-typedef _FgbAsyncResDefDart{{$f.PascalName}} = _FgbRet{{$f.PascalName}} Function(int);
-typedef _FgbAsyncResDefC{{$f.PascalName}} = _FgbRet{{$f.PascalName}} Function(ffi.Uint64);
+ffi.Uint64)>(symbol: "fgbasync_{{$f.SnakeName}}")
+external void _fgbAsync{{$f.PascalName}}(
+{{- range $i, $p := $f.Params}}{{- if eq $p.DartMode "direct" -}}
+  {{$p.DartType}}
+{{- else if eq $p.DartMode "map" -}}
+  {{$p.DartCType}}
+{{- else -}}
+  unknown
+{{- end}} arg{{$i}}, {{end -}}
+int argPtr);
+
+@ffi.Native<_FgbRet{{$f.PascalName}} Function(ffi.Uint64)>(symbol: "fgbasyncres_{{$f.SnakeName}}")
+external _FgbRet{{$f.PascalName}} _fgbAsyncRes{{$f.PascalName}}(int arg0);
 {{- end}}
 
 final class _FfiBridge implements Bridge {
   late _GoAllocator _allocator;
-{{- range $f := $top.Functions}}
-  late _FgbDefDart{{$f.PascalName}} _{{$f.CamelName}}Ptr;
-  late _FgbAsyncDefDart{{$f.PascalName}} _{{$f.CamelName}}PtrAsync;
-  late _FgbAsyncResDefDart{{$f.PascalName}} _{{$f.CamelName}}PtrAsyncRes;
-{{- end}}
-
-{{- range $s := $top.ValueStructs}}
-  late _FgbEmpty{{$s.PascalName}} _empty{{$s.PascalName}}Ptr;
-{{- end}}
 
 {{- range $s := $top.RefStructs}}
-  late ffi.Pointer<ffi.NativeFinalizerFunction> _free{{$s.PascalName}}Ptr;
   late ffi.NativeFinalizer _{{$s.CamelName}}Finalizer;
 {{- end}}
 
-  _FfiBridge(ffi.DynamicLibrary lib) {
-    var allocPtr = lib.lookupFunction<_FgbDefIntCAlloc, _FgbDefIntDartAlloc>("fgbinternal_alloc");
-    var freePtr = lib.lookupFunction<_FgbDefIntCFree, _FgbDefIntDartFree>("fgbinternal_free");
-    _allocator = _GoAllocator(allocPtr, freePtr);
+  _FfiBridge() {
+    _allocator = const _GoAllocator();
 
-    var initPtr = lib.lookupFunction<_FgbDefInit, _FgbDefInit>("fgbinternal_init");
-    var initRes = initPtr(ffi.NativeApi.initializeApiDLData);
+    var initRes = _fgbInternalInit(ffi.NativeApi.initializeApiDLData);
     if (initRes != ffi.nullptr) {
       var errPtr = ffi.Pointer<Utf8>.fromAddress(initRes.address);
       var errMsg = errPtr.toDartString(); 
@@ -215,16 +208,7 @@ final class _FfiBridge implements Bridge {
 
       throw BridgeException(errMsg);
     }
-{{range $f := $top.Functions}}
-    _{{$f.CamelName}}Ptr = lib.lookupFunction<_FgbDefC{{$f.PascalName}}, _FgbDefDart{{$f.PascalName}}>("fgb_{{$f.SnakeName}}");
-    _{{$f.CamelName}}PtrAsync = lib.lookupFunction<_FgbAsyncDefC{{$f.PascalName}}, _FgbAsyncDefDart{{$f.PascalName}}>("fgbasync_{{$f.SnakeName}}");
-    _{{$f.CamelName}}PtrAsyncRes = lib.lookupFunction<_FgbAsyncResDefC{{$f.PascalName}}, _FgbAsyncResDefDart{{$f.PascalName}}>("fgbasyncres_{{$f.SnakeName}}");
-{{- end}}
-{{range $s := $top.ValueStructs}}
-    _empty{{$s.PascalName}}Ptr = lib.lookupFunction<_FgbEmpty{{$s.PascalName}}, _FgbEmpty{{$s.PascalName}}>("fgbempty_{{$s.SnakeName}}");
-{{- end}}
 {{range $s := $top.RefStructs}}
-    _free{{$s.PascalName}}Ptr = lib.lookup<ffi.NativeFinalizerFunction>("fgbfree_{{$s.SnakeName}}");
     _{{$s.CamelName}}Finalizer = ffi.NativeFinalizer(_free{{$s.PascalName}}Ptr);
 {{- end}}
   }
@@ -233,16 +217,16 @@ final class _FfiBridge implements Bridge {
   {{if $f.HasRes}}{{$f.Res.DartType}}{{else}}void{{end}} {{$f.CamelName}}(
     {{- range $i, $p := $f.Params}}{{if gt $i 0}}, {{end}}{{$p.DartType}} {{$p.Name}}{{end -}}
   ) {
-    {{- range $p := $f.Params}}
+    {{- range $p := $f.Params -}}
     {{- if eq $p.DartMode "direct"}}
     var __Dart__{{$p.Name}} = {{$p.Name}};
     {{- else if eq $p.DartMode "map"}}
     var __Dart__{{$p.Name}} = _mapFrom{{$p.MapName}}({{$p.Name}});
     {{- else}}
     var __Dart__{{$p.Name}} = unknown;
-    {{- end}}
+    {{- end -}}
     {{end}}
-    {{if $f.HasRes}}return {{end}}_process{{$f.PascalName}}(_{{$f.CamelName}}Ptr(
+    {{if $f.HasRes}}return {{end}}_process{{$f.PascalName}}(_fgb{{$f.PascalName}}(
       {{- range $i, $p := $f.Params}}{{if gt $i 0}}, {{end}}__Dart__{{$p.Name}}{{end -}}
     ));
   }
@@ -251,22 +235,22 @@ final class _FfiBridge implements Bridge {
   Future<{{if $f.HasRes}}{{$f.Res.DartType}}{{else}}void{{end}}> {{$f.CamelName}}Async(
     {{- range $i, $p := $f.Params}}{{if gt $i 0}}, {{end}}{{$p.DartType}} {{$p.Name}}{{end -}}
   ) async {
-    {{- range $p := $f.Params}}
+    {{- range $p := $f.Params -}}
     {{- if eq $p.DartMode "direct"}}
     var __Dart__{{$p.Name}} = {{$p.Name}};
     {{- else if eq $p.DartMode "map"}}
     var __Dart__{{$p.Name}} = _mapFrom{{$p.DartType}}({{$p.Name}});
     {{- else}}
     var __Dart__{{$p.Name}} = unknown;
-    {{- end}}
+    {{- end -}}
     {{end}}
     var __DartRecv__ = ReceivePort('AsyncRecv({{$f.CamelName}})');
-    _{{$f.CamelName}}PtrAsync(
+    _fgbAsync{{$f.PascalName}}(
       {{- range $p := $f.Params}}__Dart__{{$p.Name}}, {{end -}}
     __DartRecv__.sendPort.nativePort);
     var __DartMsg__ = await __DartRecv__.first;
     __DartRecv__.close();
-    {{if $f.HasRes}}return {{end}}_process{{$f.PascalName}}(_{{$f.CamelName}}PtrAsyncRes(__DartMsg__[0]));
+    {{if $f.HasRes}}return {{end}}_process{{$f.PascalName}}(_fgbAsyncRes{{$f.PascalName}}(__DartMsg__[0]));
   }
 
   {{if $f.HasRes}}{{$f.Res.DartType}}{{else}}void{{end}} _process{{$f.PascalName}}(_FgbRet{{$f.PascalName}} res) {
@@ -288,7 +272,7 @@ final class _FfiBridge implements Bridge {
     {{- end}}
     {{- end}}
   }
-{{- end -}}
+{{end -}}
 {{- range $s := $top.ValueStructs}}
 
   {{$s.PascalName}} _mapTo{{$s.PascalName}}(_FgbC{{$s.PascalName}} from) {
@@ -307,7 +291,7 @@ final class _FfiBridge implements Bridge {
   }
 
   _FgbC{{$s.PascalName}} _mapFrom{{$s.PascalName}}({{$s.PascalName}} from) {
-    var res = _empty{{$s.PascalName}}Ptr();
+    var res = _fgbEmpty{{$s.PascalName}}();
     {{- range $f := $s.Fields}}
     {{- if eq $f.DartMode "direct"}}
     res.{{$f.CamelName}} = from.{{$f.CamelName}};
